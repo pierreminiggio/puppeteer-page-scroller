ad857a3bc6c2fec472822bb78aac4c9bc26a4be5
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-12-05 14:19:59 +0100

    first commit

--- main.js
+++ main.js
@@ -5,10 +5,12 @@
  * @returns {void}
  */
 export default async function scroll(page, maxScroll) {
-    await page.evaluate(async (maxScroll) => {
-        await new Promise((resolve) => {
+    await page.evaluate(async maxScroll => {
+        await new Promise(resolve => {
+
             const distance = 100
             let scrolledDistance = 0
+            
             const timer = setInterval(() => {
 
                 window.scrollBy(0, distance)
