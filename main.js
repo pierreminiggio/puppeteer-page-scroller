79d35db6967fe71d0f5603de01addffacca9c8e5
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-12-05 14:18:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+/**
+ * @param {import("puppeteer").Page} page 
+ * @param {number} maxScroll 
+ * 
+ * @returns {void}
+ */
+export default async function scroll(page, maxScroll) {
+    await page.evaluate(async (maxScroll) => {
+        await new Promise((resolve) => {
+            const distance = 100
+            let scrolledDistance = 0
+            const timer = setInterval(() => {
+
+                window.scrollBy(0, distance)
+                scrolledDistance += distance
+
+                if (scrolledDistance >= maxScroll) {
+                    clearInterval(timer)
+                    resolve()
+                }
+
+            }, 100)
+        })
+    }, maxScroll)
+}
