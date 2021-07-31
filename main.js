0d8a09962eafd7aedbc2ec65864f4ba0b2a31d76
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-31 19:40:37 +0200

    allow element to get scrolled

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 /**
  * @param {import("puppeteer").Page} page 
- * @param {number} maxScroll 
+ * @param {number} maxScroll
+ * @param {?string} selector Will default to window
  * 
  * @returns {void}
  */
-export default async function scroll(page, maxScroll) {
-    await page.evaluate(async maxScroll => {
+export default async function scroll(page, maxScroll, selector = null) {
+    await page.evaluate(async (maxScroll, selector) => {
         await new Promise(resolve => {
 
+            const elementToScroll = selector ? document.querySelector(selector) : window
             const distance = 100
             let scrolledDistance = 0
             
             const timer = setInterval(() => {
 
-                window.scrollBy(0, distance)
+                elementToScroll.scrollBy(0, distance)
                 scrolledDistance += distance
 
                 if (scrolledDistance >= maxScroll) {
@@ -23,5 +25,5 @@ export default async function scroll(page, maxScroll) {
 
             }, 100)
         })
-    }, maxScroll)
+    }, maxScroll, selector)
 }
